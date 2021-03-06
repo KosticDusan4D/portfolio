d412194ac54e103e517af569fab990163a69076b
KosticDusan4D <75327101+KosticDusan4D@users.noreply.github.com>
2021-03-06 12:25:42 +0100

    Add files via upload
    This is just a simple demonstration of HTML and CSS including Bootstrap 4 responsive design.

---
+++ html_css(bootstrap_responsive)/dusankostic.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dusan-kostic.css">
+    <title>Dušan Kostić</title>
+</head>
+<body>
+    <div class="row" id="div1">
+        <div class="col-md-12">
+            <div class="row no-gutters">
+                    <img class="col-md-1 text-center no-gutters" src="https://i.ibb.co/Rb1vBhg/logo.png" alt="logo">
+            </div>
+            <div class="row no-gutters">
+                <h2 class="col-md-12 text-center no-gutters text">
+                    SVI ZAJEDNO
+                </h2>
+            </div>
+            <div class="row no-gutters">
+                <p class="col-md-12 text-center no-gutters">
+                    Zajednica izuzetnih profesionalaca koji su tu da ti pomognu da i ti<br>postaneš to. Od tebe očekujemo samo da i ti pomogneš drugima.
+                </p>
+            </div>
+            <div class="row no-gutters">
+                <a class="col-md-5 text-center justify-content-center btn btn-light font-weight-bold"" href="#">TRAŽIM POSAO <span class="text-primary">&|</span> ZNANJE</a>
+            </div>
+            <div class="row no-gutters">
+                <img class="col-md-12 m-r-auto m-l-auto" src="https://i.ibb.co/Y0V8S5b/slika.png" alt="">
+            </div>
+        </div>
+    </div>
+    
+    <div class="row" id="div2">
+        <div class="col-md-12">
+            <div class="row no-gutters display-4 font-weight-bold" id="marginTop">
+                <p class="col-md-12 text-center no-gutters">
+                    Profesionalci i<br>stvaraoci budućnosti
+                </p>
+            </div>
+            <div class="row no-gutters">
+                <p class="col-md-12 text-center no-gutters">
+                    Našom platformom pružamo i unapređujemo veštine za budućnost, sa<br>kojima se može poslovaci u svom gradu ili globalno, lično ili od kuće.
+                </p>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-4 text-right no-gutters" id="div2text">
+                    <h5>INDIVIDUALNI PRISTUP</h5>
+                    <p>
+                        Korisnicima platforme naši savetnici i mentori pomažu da definišu naredni karijerni korak, bilo da je u pitanju prvi posao, prekvalifikacija ili usavršavanje postojećih veština, i pomažu im na putu učenja i tržišnog nastupa.
+                    </p> 
+                </div>
+                <div class="col-md-4 no-gutters">
+                    <div id="first" class="circle"></div>
+                    <div id="second" class="circle"></div>
+                    <div id="third" class="circle"></div>
+                </div>
+                <div class="col-md-4 text-left no-gutters" id="div2text">
+                    <h5>OBRAZOVNI MATERIJALI</h5>
+                    <p>
+                        Tokom vremena, na ovoj platformi ćemo akumulirati i kreirati probrane i prilagođene obrazovne materijale za sve oblasti privrede budućnosti, od osnovnih znanja do usko stručnih tema.
+                    </p> 
+                </div>
+                <div class="row no-gutters"></div>
+                    <div class="col-md-4 no-gutters divCenter">
+                        <h5 class="text-center">JEDNI ZA DRUGE</h5>
+                        <p  class="text-center">
+                            Svo znanje i mentorstvo na platformi je besplatno za krajnje korisnike, dok god prihvataju naše vrednosti, među kojima je najvažnija to da nesebično pomažu drugima kada su u mogućnosti.
+                        </p> 
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="div3">
+        <div class="col-md-12">
+            <div class="row no-gutters">
+                <h3 class="col-md-12 text-center no-gutters">
+                    OSNOVNA I TRŽIŠNA ZNANJA
+                </h3>
+            </div>
+            <div class="row no-gutters">
+                <p class="col-md-5 text-center no-gutters  divCenter">
+                Korisnici platforme dobijaju pomoć savetnika i drugih korisnika da steknu razna znanja potrebna tržištu, od najosnovnijih do uskostručnih.
+                </p>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-4 text-center no-gutters">
+                    <img src="https://i.ibb.co/bdPJZq2/ico-hands.png" alt="icon_hands">
+                    <br>
+                    <h4 class="font-weight-bold">Digitalna pismenost</h4>
+                    <br>
+                    <p>
+                        Komotno baratanje računarima, korišćenje interneta i raznog softvera je bazno znanje.
+                    </p>
+                </div>              
+                <div class="col-md-4 text-center no-gutters">
+                    <img src="https://i.ibb.co/6ss8GBQ/ico-pos-Kom.png" alt="icon"> 
+                    <br>
+                    <h4 class="font-weight-bold">Poslovna komunikacija</h4>
+                    <br>
+                    <p>
+                        Sposobnost poslovne komunikacije je nužna veštin globalnih profesionalaca i stvaralaca.
+                    </p>
+                </div>
+                <div class="col-md-4 text-center no-gutters">
+                    <img src="https://i.ibb.co/TRfQ3K3/ico-eng.png" alt="icon eng"> 
+                    <br>
+                    <h4 class="font-weight-bold">Engleski jezik</h4>
+                    <br>
+                    <p>
+                        Za globalne prilike moramo znati da se samouvereno služimo globalnim jezikom.
+                    </p>
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-12" id="border"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/WtkJhQs/asistent.png" alt="icon eng">
+                    <br>
+                    <h6>OSNOVNO POMAGANJE</h6>
+                </div>
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/n6QPRgM/teacher.png" alt="icon eng">
+                    <br>
+                    <h6>PODUČAVANJE</h6>
+                </div>
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/L0jWRCH/marketing.png" alt="icon eng">
+                    <br>
+                    <h6>MARKETING I PRODAJA</h6>
+                </div>
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/B6nf3Jq/IT.png" alt="icon eng">
+                    <br>
+                    <h6>TEHNOLOŠKA INDUSTRIJA</h6>
+                </div>
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/gZ7Qyd8/bm.png" alt="icon eng">
+                    <br>
+                    <h6>UPRAVLJANJE POSLOVANJEM</h6>
+                </div>
+                <div class="col-md-2 text-center no-gutters">
+                    <img src="https://i.ibb.co/tqdqxFZ/entrepr.png" alt="icon eng">
+                    <br>
+                    <h6>PREDUZETNIŠTVO</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="div4">
+        <div class="col-md-12">
+            <div class="row no-gutters">
+                <h3 class="col-md-12 text-center no-gutters">
+                    PROCES RADA
+                </h3>
+            </div>
+            <div class="row no-gutters">
+                <div class="col text-center no-gutters">
+                    <img src="https://i.ibb.co/4KY06bT/aplikacija.png" alt="aplikacija" width="50px">
+                </div>
+                <div class="col text-center no-gutters">
+                    <img src="https://i.ibb.co/tDKqN5R/plan.png" alt="plan" width="50px">
+                </div>
+                <div class="col text-center no-gutters">
+                    <img src="https://i.ibb.co/m05xbxv/ucenje.png" alt="ucenje" width="50px">
+                </div>
+                <div class="col text-center no-gutters">
+                    <img src="https://i.ibb.co/7S4cDK4/cv.png" alt="cv" width="50px">
+                </div>
+                <div class="col text-center no-gutters">
+                    <img src="https://i.ibb.co/DQHgqx8/pomo-drugima.png" alt="pomo-drugima" width="50px">
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md-12" id="border"></div>
+            </div>
+            <div class="row no-gutters">
+                <div class="col text-center no-gutters">
+                    <p>
+                        Korisnik se prijavljuje za platformu, i ukoliko prihvata i poštuje pravila dobija pristup i postavlja prvi zahtev za pomoć edukaciji.
+                    </p>
+                </div>
+                <div class="col text-center no-gutters">
+                    <p>
+                        Uz pomoć karijernog savetnika korisnik postavlja ciljeve učenja na platformi, po potrebi (re)definiše karijerni put.
+                    </p>
+                </div>
+                <div class="col text-center no-gutters">
+                    <p>
+                        Uz pomoć savetnika korisnik savlađuje materijale, radi zadatke, dobija pomoć u učenju  i 1-1 mentorske sesije sa ekspertima.
+                    </p>
+                </div>
+                <div class="col text-center no-gutters">
+                    <p>
+                        Ukoliko je to bilo među ciljevima, savetnici pomažu u kreiranju plan za nastup na tržištu - bilo zarad zaposlenja ili razvoja startapa.
+                    </p>
+                </div>
+                <div class="col text-center no-gutters">
+                    <p>
+                        Korisnici ne plaćaju ništa - samo obećavaju da će čim pre pomagati drugim korisnicima platforme da rastu i razvijaju se.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" id="div5">
+        <div class="col-md-12">
+            <div class="row no-gutters">
+                <span class="col-md-12 text-center no-gutters">
+                    Naš tim
+                </span>
+            </div>
+            <div class="row no-gutters">
+                <p class="col-md-5 text-center no-gutters divCenter">
+                    Korisnicima u radu na sticanju znanja pomažu naši karijerni, obrazovni i savetnici za tržišni nastup, oslonjeni na pomoć drugih korisnika i usko-stručnih mentora.
+                </p>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-md text-center no-gutters divCenter">
+                </div>
+                <div class="col text-center no-gutters divCenter">
+                    <img src="https://i.ibb.co/Dr7Xv6L/ivana.png" alt="ivana" width="175px">
+                    <h3>Ivana Andrašević</h3>
+                </div>
+                <div class="col-md text-center no-gutters divCenter">
+                    <img src="https://i.ibb.co/2qsd31j/tamara.png" alt="tamara" width="175px">
+                    <h3>Tamara Stanojević</h3>
+                </div>
+                <div class="col-md text-center no-gutters divCenter">
+                    <img src="https://i.ibb.co/1GBk9Vd/gojko.png" alt="gojko" width="175px">
+                    <h3>Gojko Radišić</h3>
+                </div>
+                <div class="col text-center no-gutters divCenter">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
